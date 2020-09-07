f804bae7a694cf44ab8bde4ec19867a837c47fb9
AtomicZero <atomiczero@gmail.com>
2020-09-07 22:17:23 +0100

    Updates

---
+++ precache-manifest.8cff4dc30ba9519394c62c437de84a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c69cd4dd7a96921226b550e38ae761",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b958881de347ee748b37",
+    "url": "/google-books-client/static/js/main.1bdd4b25.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
